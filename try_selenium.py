e5006e31730f5a17c0b4799bff05a729a2ab8629
akfvhrl <akfvhrl@konkuk.ac.kr>
2020-04-13 12:35:09 +0900

    add time, schedule and use several stock

--- try_selenium.py
+++ try_selenium.py
@@ -1,33 +1,67 @@
+import schedule
+
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
 import time
 
-# 셀레니움을 실행하는데 필요한 크롬드라이버 파일을 가져옵니다.
-driver = webdriver.Chrome('./chromedriver')
 
-# 네이버 주식페이지 url을 입력합니다.
-url = 'https://m.stock.naver.com/item/main.nhn#/stocks/005930/total'
+def get_my_stock():
+    ### option 적용 ###
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("window-size=1920x1080")
+    options.add_argument("disable-gpu")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    )
 
-# 크롬을 통해 네이버 주식페이지에 접속합니다.
-driver.get(url)
+    driver = webdriver.Chrome("chromedriver", options=options)
+    ##################
 
-# 정보를 받아오기까지 2초를 잠시 기다립니다.
-time.sleep(2)
+    codes = ["005930", "035420", "017670", "096770", "035720"]
 
-# 크롬에서 HTML 정보를 가져오고 BeautifulSoup을 통해 검색하기 쉽도록 가공합니다.
-soup = BeautifulSoup(driver.page_source, 'html.parser')
+    for code in codes:
+        # 네이버 주식페이지 url을 입력합니다.
+        url = "https://m.stock.naver.com/item/main.nhn#/stocks/" + code + "/total"
 
-name = soup.select_one(
-    '#header > div.end_header_topinfo > div.flick-container.major_info_wrp > div > div:nth-child(2) > div > div.item_wrp > div > h2').text
+        # 크롬을 통해 네이버 주식페이지에 접속합니다.
+        driver.get(url)
 
-current_price = soup.select_one(
-    '#header > div.end_header_topinfo > div.flick-container.major_info_wrp > div > div:nth-child(2) > div > div.stock_wrp > div.price_wrp > strong').text
+        # 정보를 받아오기까지 2초를 잠시 기다립니다.
+        time.sleep(2)
 
-rate = soup.select_one(
-    '#header > div.end_header_topinfo > div.flick-container.major_info_wrp > div > div:nth-child(2) > div > div.stock_wrp > div.price_wrp > div > span.gap_rate > span.rate').text
+        # 크롬에서 HTML 정보를 가져오고 BeautifulSoup을 통해 검색하기 쉽도록 가공합니다.
+        soup = BeautifulSoup(driver.page_source, "html.parser")
 
-print(name, current_price, rate)
+        name = soup.select_one(
+            "#header > div.end_header_topinfo > div.flick-container.major_info_wrp > div > div:nth-child(2) > div > div.item_wrp > div > h2"
+        ).text
 
-# 크롬을 종료합니다.
-driver.quit()
+        current_price = soup.select_one(
+            "#header > div.end_header_topinfo > div.flick-container.major_info_wrp > div > div:nth-child(2) > div > div.stock_wrp > div.price_wrp > strong"
+        ).text
+
+        rate = soup.select_one(
+            "#header > div.end_header_topinfo > div.flick-container.major_info_wrp > div > div:nth-child(2) > div > div.stock_wrp > div.price_wrp > div > span.gap_rate > span.rate"
+        ).text
+
+        print(name, current_price, rate)
+
+    print("-------")
+    # 크롬을 종료합니다.
+    driver.quit()
+
+
+def job():
+    get_my_stock()
+
+
+def run():
+    schedule.every(15).seconds.do(job)  # 15초에 한번씩 실행
+    while True:
+        schedule.run_pending()
+
+
+if __name__ == "__main__":
+    run()
