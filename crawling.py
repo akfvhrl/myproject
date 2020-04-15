94483c7d8505d7c859d3d99f9d8ec89248a8ca32
akfvhrl <akfvhrl@konkuk.ac.kr>
2020-04-15 16:43:09 +0900

    find codes and get only 20 stocks

--- crawling.py
+++ crawling.py
@@ -14,15 +14,19 @@ data = requests.get("https://finance.naver.com/sise/sise_market_sum.nhn", header
 soup = BeautifulSoup(data.text, "html.parser")
 
 stocks = soup.select("table.type_2 > tbody > tr")
-
 delete = db.stocks.delete_many({})
 delete.deleted_count
 
+cnt = 0
 for stock in stocks:
     a_title = stock.select_one("td > a")
     if a_title is not None:
+        cnt += 1
         title = a_title.text
         share = stock.select_one("td.number").text
-        
-        doc = {"title": title, "share": share, "variance": 0}
+        full_code = a_title["href"].split("=")
+        code = full_code[-1]
+        doc = {"title": title, "code": code, "share": share, "variance": 0, "variance_rate": 0}
         db.stocks.insert_one(doc)
+        if cnt == 20:
+            break
