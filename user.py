85b3d3b7e9c6c8ddc45ed1e19699a7a65d4c6045
akfvhrl <akfvhrl@konkuk.ac.kr>
2020-04-18 16:24:05 +0900

    add useage after login in console

--- user.py
+++ user.py
@@ -5,6 +5,7 @@ db = client.dbsparta
 
 email = input()
 password = input()
+is_login = 0
 
 a = db.users.find_one({"email": email})
 
@@ -23,5 +24,20 @@ if a is None:
 else:
     if a["password"] is password:
         print("로그인")
+        is_login = 1
     else:
         print("비밀번호가 틀렸습니다.")
+
+while is_login is 1:
+    todo = input()
+    if todo == "buy":
+        lists = db.stocks
+        for element in lists.find({}, {"_id": 0}):
+            print(element)
+        to_buy = input()
+        element = lists.find_one({"title": to_buy}, {"_id": 0})
+        print(element)
+        // user db update
+    if todo == "logout":
+        is_login = 0
+        print("logout")
