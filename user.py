b9cac87cc119c5a01764af470b61a48b67b3d3f9
akfvhrl <akfvhrl@konkuk.ac.kr>
2020-04-19 18:54:06 +0900

    user console db update

--- user.py
+++ user.py
@@ -8,6 +8,7 @@ password = input()
 is_login = 0
 
 a = db.users.find_one({"email": email})
+users = db.users
 
 if a is None:
     user = input("이름을 입력해주세요")
@@ -35,9 +36,39 @@ while is_login is 1:
         for element in lists.find({}, {"_id": 0}):
             print(element)
         to_buy = input()
+        buy_cnt = int(input())
         element = lists.find_one({"title": to_buy}, {"_id": 0})
         print(element)
+        users.update_one(
+            {"email": email},
+            {
+                "$set": {
+                    "get_stocks": [element["title"], buy_cnt],
+                    "money": (
+                        users.find_one({"email": email})["money"]
+                        - int(element["share"].replace(",", ""))
+                    )
+                    * buy_cnt,
+                }
+            },
+        )
         # user db update
+    if todo == "sale":
+        for user in users.find({"email": email}, {"_id": 0, "get_stocks": 1}):
+            print(user)
+        to_sale = input()
+        users.update_one(
+            {"email": email},
+            {
+                "$set": {
+                    "money": users.find_one({"email": email})["money"]
+                    + users.find_one({"email": email})["get_stocks"][1],
+                    "get_stocks": [],
+                }
+            },
+        )
+    if todo == "money":
+        print(users.find_one({"email": email})["money"])
     if todo == "logout":
         is_login = 0
         print("logout")
