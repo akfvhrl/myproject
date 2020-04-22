b04ba8d9f5c75a608d6254aa6a19226f4955be58
akfvhrl <akfvhrl@konkuk.ac.kr>
2020-04-22 22:07:25 +0900

    add get method

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
+client = MongoClient("127.0.0.1", 27017)
+db = client.dbsparta
 
 app = Flask(__name__)
 
@@ -11,6 +15,13 @@ def home():
 @app.route("/main")
 def main():
     return render_template("main.html")
+
+
+@app.route("/main/stock", methods=["GET"])
+def get_stock():
+    code = request.form["code"]
+    stock = db.stocks.find_one({"code": code}, {"_id": 0})
+    return jsonify({"result": "success", "msg": "저장되었습니다.", "data": stock})
 
 
 @app.route("/mypage")
